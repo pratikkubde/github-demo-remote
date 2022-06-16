d42a1f96f8df30da297c23266427971a56f92fc2
pratikkubde <pratikkubde4@gmail.com>
2022-06-16 10:54:26 +0530

    modified into simple file to make further changes

--- math.py
+++ math.py
@@ -1,18 +1,12 @@
 #add fn
 def add (x,y)
-    return (x+y)
+    pass
 #sub fn
 def subtract(x,y)
-    return (x-y)
+    pass
 #mul fn
 def multiply(x,y)
-    return (x*y)
+    pass
 #div fn
 def divide(x,y)
-    if(y==0)                    # On master branch
-        return DIVIDE_BY_0_ERROR
-    else
-        return x/y
-#Square Implementation
-def square(x)
-    return x*x
+    pass
